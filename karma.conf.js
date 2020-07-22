1b5aa7a5f82c7adfb03f31acf006f033b84ac585
ramyrams <email2rams@yahoo.com>
2020-07-22 13:01:38 -0500

    Initial

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -31,6 +32,7 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true,
+    reporters: ['progress', 'kjhtml', 'junit'],
     browsers: ['ChromeHeadless']
   });
 };
